ca872b3a11d4c7ad12b8fbe8452daa55cd455b5a
Pranav Tambat <pranavtambat2000@gmail.com>
2021-11-16 14:45:43 +0530

    Message Center App demo

---
+++ MessageCenterDemoApp/MessageCenterDemoApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MessageCenterDemoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Messages { get; set; } = new ObservableCollection<string>();
+        public MainPage()
+        {
+            
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        public void Button_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Subscribe<Page>(this, "rada", (p) =>
+            {
+                Messages.Add($"Message Received");
+            });
+        }
+
+        async public void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Page1());
+        }
+    }
+}
---
+++ MessageCenterDemoApp/MessageCenterDemoApp/Page1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MessageCenterDemoApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send<Page>(this, "rada");
+        }
+
+       async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+    }
+}
